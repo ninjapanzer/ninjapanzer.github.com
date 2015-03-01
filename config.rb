f708461f6da866af1f9200325c7680db38640877
Paul Scarrone <pscarrone@thinkthroughmath.com>
2015-03-01 11:21:45 -0500

    blog base

--- config.rb
+++ config.rb
@@ -51,9 +51,9 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   blog.prefix = "blog"
 
-  blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "{year}/{month}/{day}/{title}"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
   blog.layout = "blog"
   # blog.summary_separator = /(READMORE)/
@@ -63,14 +63,18 @@ activate :blog do |blog|
   # blog.day_link = "{year}/{month}/{day}.html"
   # blog.default_extension = ".markdown"
 
-  blog.tag_template = "tag.html"
-  blog.calendar_template = "calendar.html"
+  blog.tag_template = "blog/tag.html"
+  blog.calendar_template = "blog/calendar.html"
 
   # Enable pagination
   # blog.paginate = true
   # blog.per_page = 10
   # blog.page_link = "page/{num}"
 end
+
+set :url_root, 'http://localhost:4567'
+
+set :haml, { :ugly => true, :format => :html5 }
 
 set :css_dir, 'stylesheets'
 
